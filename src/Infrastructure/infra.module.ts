2dad4e1ae607116e441f99cfc133447d12ce51ac
Tiago Landim <tortoise.tl@gmail.com>
2024-11-10 19:14:51 -0300

    📦 adicionados pacotes de passport

--- src/Infrastructure/infra.module.ts
+++ src/Infrastructure/infra.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './connections/database.module';
+import { PasswordService } from './password/password.service';
 
 @Module({
   imports: [DatabaseModule],
+  providers: [PasswordService],
+  exports: [PasswordService],
 })
 export class InfraModule {}
